9bad549f90edb389b7b13dfa999373abec66656b
RyotaAraya <58146059+RyotaAraya@users.noreply.github.com>
2025-09-27 20:40:51 +0900

    chore: 1-2 vue.js 導入

---
+++ frontend/components/HelloVue.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref("こんにちは！Vue + Rails + Vite")
+</script>
+
+<template>
+  <div>
+    <h2>{{ message }}</h2>
+    <button @click="message = 'クリックされました！'">クリック</button>
+  </div>
+</template>
+
+<style>
+h2 {
+  color: blue;
+}
+</style>
---
+++ frontend/entrypoints/application.js
@@ -0,0 +1,15 @@
+// To see this message, add the following to the `<head>` section in your
+// views/layouts/application.html.erb
+//
+//    <%= vite_client_tag %>
+//    <%= vite_javascript_tag 'application' %>
+
+import { createApp } from 'vue'
+import HelloVue from '../components/HelloVue.vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const el = document.getElementById('vue-app')
+  if (el) {
+    createApp(HelloVue).mount(el)
+  }
+})
