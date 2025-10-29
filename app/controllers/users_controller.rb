2dbb91d8ff1386b6c58c06e8509227a6382b5fe8
RyotaAraya <58146059+RyotaAraya@users.noreply.github.com>
2025-10-30 05:40:03 +0900

    chore: design調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   private
 
   def set_user
-    @user = User.find(params[:id])
+    @user = User.includes(:department, :roles).find(params[:id])
   end
 
   def set_target_date
